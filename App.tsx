39f7128e2f5b12eceae61dfb58d35918b589f5b4
JrzenonDev <jrobertoonb@gmail.com>
2022-03-21 22:00:28 -0300

    creat and configure component linear-gradient

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { useFonts } from 'expo-font'
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani'
 import AppLoading from 'expo-app-loading'
+import { Background } from './src/components/Backgroun'
 
 export default function App () {
   const [fontLoaded] = useFonts({
@@ -21,13 +22,13 @@ export default function App () {
   }
 
   return (
-    <>
+    <Background>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <SignIn />
-    </>
+    </Background>
   )
 }
