e723213f31c24d3d489f7149a95d09489a1882a9
JrzenonDev <jrobertoonb@gmail.com>
2022-03-21 22:04:34 -0300

    rename folder component Background

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import { useFonts } from 'expo-font'
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani'
 import AppLoading from 'expo-app-loading'
-import { Background } from './src/components/Backgroun'
+import { Background } from './src/components/Background'
 
 export default function App () {
   const [fontLoaded] = useFonts({
